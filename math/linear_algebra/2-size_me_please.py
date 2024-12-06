1b63dcd40e2280465bfc1a422de1032564f06bd6
Lyan-OS <lyanosorio98@gmail.com>
2024-12-06 10:34:47 -0400

    added documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+
+
 """calculates shape of matrix"""
+
+
 def matrix_shape(matrix):
+    """Calculates the shape of a matrix."""
     shape = []
 
     while isinstance(matrix, list):
