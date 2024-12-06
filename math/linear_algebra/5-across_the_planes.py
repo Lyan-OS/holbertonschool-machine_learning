d71c705e93879cce09e1a9031dd3c9fcf8118af9
Lyan-OS <lyanosorio98@gmail.com>
2024-12-06 17:47:37 -0400

    added 5-across_the_planes.py

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""function that adds two matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """adds two matrices element wise
+    Returns: new list"""
+
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+
+    full = []
+    for i in range(len(mat1[0])):
+        new = []
+        for j in range(len(mat1)):
+            new.append(mat1[i][j] + mat2[i][j])
+        full.append(new)
+    return full
+
+
+def matrix_shape(matrix):
+    """Calculates the shape of a matrix."""
+    shape = []
+
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
