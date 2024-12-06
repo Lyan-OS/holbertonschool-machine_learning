f27efb1c222ecce4ccdc4af06ae14d53612ac505
Lyan-OS <lyanosorio98@gmail.com>
2024-12-06 14:29:47 -0400

    deleted shape and fixed blank spaces

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -3,19 +3,13 @@
 
 
 def add_arrays(arr1, arr2):
-    """adds two arrays element-wise"""
-    if matrix_shape(arr1) != matrix_shape(arr2):
+    """adds two arrays element-wise
+
+    Returns: new list"""
+    if len(arr1) != len(arr2):
         return None
+
     new = []
     for i in range(len(arr1)):
         new.append(arr1[i] + arr2[i])
     return new
-
-def matrix_shape(matrix):
-    """Calculates the shape of a matrix."""
-    shape = []
-
-    while isinstance(matrix, list):
-        shape.append(len(matrix))
-        matrix = matrix[0]
-    return shape
