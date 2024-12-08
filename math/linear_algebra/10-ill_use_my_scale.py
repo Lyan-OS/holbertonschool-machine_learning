c80ce989e02b529145442921a789574bbf52f6e2
Lyan-OS <lyanosorio98@gmail.com>
2024-12-08 08:36:07 -0400

    added script line and documentation

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+""" function def np_shape(matrix): that calculates
+the shape of a numpy.ndarray"""
+
+
 def np_shape(matrix):
     """Calculates the shape of a numpy.ndarray."""
     return matrix.shape
