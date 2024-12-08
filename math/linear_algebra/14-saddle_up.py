78ceedae39b22476008cb240035c408026c8166f
Lyan-OS <lyanosorio98@gmail.com>
2024-12-08 09:05:10 -0400

    added 14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""functions that performs matrix multiplication
+on two numpy.ndarrays"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication on two numpy.ndarrays."""
+    return np.matmul(mat1, mat2)
