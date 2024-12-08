092c6328c1cc6b3a23e04cfdb0e235ab163d877a
Lyan-OS <lyanosorio98@gmail.com>
2024-12-08 06:28:55 -0400

    added 7-gettin_cozy.py

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/venv python3
+"""concatenates two matrices along a specific axis"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """concatenates two amtrices"""
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return [row.copy() for row in mat1] + [row.copy() for row in mat2]
+    if axis == 1:
+        if len(mat[1]) != len(mat2[1]):
+            return None
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
