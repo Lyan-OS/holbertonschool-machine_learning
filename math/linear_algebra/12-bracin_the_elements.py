644ca7dae7c2f701ab146b759bdae2bdb4d26591
Lyan-OS <lyanosorio98@gmail.com>
2024-12-08 08:55:07 -0400

    deleted module import

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """function that performs element wise add, sub, mul and div"""
-import numpy as np
 
 
 def np_elementwise(mat1, mat2):
-    """Performs element-wise add, sub, mul, and div."""
+    """Performs element-wise add, sub,
+    mul, and div."""
     return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
