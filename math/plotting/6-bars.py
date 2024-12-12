bae8525beba0110d22f1eb101ad58bf94154eb10
Lyan-OS <lyanosorio98@gmail.com>
2024-12-12 11:46:33 -0400

    fixed syntax error and pycodestyle

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def bars():
     """plot a stacked bar graph"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     people = ['Farrah', 'Fred', 'Felicia']
@@ -16,7 +16,8 @@ def bars():
 
     bottom = np.zeros(3)
     for i in range(4):
-        plt.bar(people, fruit[i], bottom=bottom, color=colors[i], label=labels[i], width=0.5)
+        plt.bar(people, fruit[i], bottom=bottom,
+                color=colors[i], label=labels[i], width=0.5)
         bottom += fruit[i]
 
     plt.xlabel('People')
