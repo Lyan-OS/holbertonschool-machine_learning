00792c5065746ecdac8686011cb58d0eadbe098b
Lyan-OS <lyanosorio98@gmail.com>
2024-12-15 16:52:13 -0400

    fixed invalid n conditional

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,7 +5,7 @@
 def summation_i_squared(n):
     """calculates sigma notation i^2"""
     
-    if isinstance(n, int) and n >= 0:
+    if isinstance(n, int) and n > 0:
         return int((n * (1 + n)) * (2 * n + 1) / 6)
     else:
         return None
